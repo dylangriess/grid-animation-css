5263b31bc131f780266562d71f696173f68febe5
dylangriess <dylangriess@gmail.com>
2022-11-20 11:45:19 -0700

    adding initial tile structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let columns = Math.floor(document.body.clientWidth / 50),
+    rows = Math.floor(document.body.clientHeight / 50);
+
+  const createTile = (index) => {
+    const tile = document.createElement("div");
+
+    tile.classList.add("tile");
+
+    return tile;
+  };
+  return <div className="App"></div>;
 }
 
 export default App;
